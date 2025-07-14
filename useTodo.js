0c7ed809491cc655e92f05126216c28a7ef73ee7
Rower <rowerguamanacaro.095@hotmail.com>
2025-07-14 09:10:02 -0500

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,53 @@
+import { useReducer, useEffect } from "react";
+import { todoReducer } from "../08-useReduce/todoReducer";
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+ const [ todos, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+
+        localStorage.setItem('todos', JSON.stringify(todos))
+      
+    }, [todos]);    
+
+    const handNewTodo = ( todo ) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = ( id ) =>{
+        dispatch({
+            type:'[TODO] Remove Todo',
+            payload : id
+        })
+    };
+
+    const handleToggleTodo = (id) => {        
+        dispatch({
+            type:'[TODO] Toggle Todo',
+            payload : id
+        })
+    }
+
+    const todosCount = todos.length;
+
+    const pendingTodosCount = todos.filter( todo => !todo.done).length;
+
+    return{
+        todos,
+        todosCount,
+        pendingTodosCount,
+        handNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+}
